64a82d7c80a7f5912e4e0bee7111b8a809379a34
Natalia Chicherova <natalia.chicherova@phystech.edu>
2018-11-06 14:56:49 +0100

    Update NumberObjectsPerImage.m

--- NumberObjectsPerImage.m
+++ NumberObjectsPerImage.m
@@ -1,10 +1,10 @@
-function T = NumberObjectsPerImage(read_dir)
+function [Ttotal,TOpt] = NumberObjectsPerImage(read_dir)
 A = readtable(fullfile(read_dir,'Allpositions_filter3D.txt'));
 frames = max(A.Frame);
 opt = max(A.Optical);
 
 k=1;
-NumObjects = zeros(1,frames*opt);
+NumObjects = zeros(frames*opt,1);
 for i=1:frames
     for j=1:opt
         NumObjects(k) = sum(A.Frame==i & A.Optical==j);
@@ -13,4 +13,13 @@ for i=1:frames
 end
 Frame = reshape(repmat(1:frames,opt,1),1,frames*opt)';
 Opt = repmat(1:opt, 1,frames)';
-T = table(Frame,Opt,NumObjects');     
+Ttotal = table(Frame,Opt,NumObjects);   
+
+k=1;
+NumObjectsOpt = zeros(opt,1);
+for j=1:opt
+    NumObjectsOpt(k) = sum(A.Optical==j);
+    k = k+1;
+end
+Opt = (1:opt)';
+TOpt = table(Opt,NumObjectsOpt);
